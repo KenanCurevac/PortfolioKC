414f88e239f97dbc188f6cc7c49b7ddbc1bf33c7
Kenan Curevac <kenancurevac@gmail.com>
2025-03-20 16:18:32 +0100

    test distance

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,23 +7,25 @@ export default function Hero(props) {
     <div className="hero-container">
       <Navbar {...props} />
       <div className="hero-content">
-        <p className="hero-pretitle" data-aos="fade-up">
-          FRONTEND SOFTWARE ENTWICKLER
-        </p>
-        <h1 className="hero-title" data-aos="zoom-in" data-aos-duration="600">
-          Hallo, ich bin{" "}
-          <span
-            style={{
-              color: "#00df9a",
-            }}
-          >
-            Kenan Ćurevac.
-          </span>
-        </h1>
+        <div className="upper-group">
+          <p className="hero-pretitle" data-aos="fade-up">
+            FRONTEND SOFTWARE ENTWICKLER
+          </p>
+          <h1 className="hero-title" data-aos="zoom-in" data-aos-duration="600">
+            Hallo, ich bin{" "}
+            <span
+              style={{
+                color: "#00df9a",
+              }}
+            >
+              Kenan Ćurevac.
+            </span>
+          </h1>
+        </div>
         <p className="hero-description" data-aos="fade-up">
           Mit Leidenschaft für Frontend-Entwicklung, entwickle ich
           benutzerfreundliche Anwendungen und freue mich auf die Zusammenarbeit
-          mit einem motivierten Team.
+          mit einem motivierten Team. lelelelel
         </p>
       </div>
     </div>
